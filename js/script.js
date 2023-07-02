3ea8ba18c44b8bd2ed85a32fde86e036b2d077fa
Luidy-Almeida-da-Silva <luidyalmeida00123@gmail.com>
2023-07-02 17:37:05 -0300

    commit 1

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const navegação = document.querySelector('.navegação');
+const menu = document.querySelector('.menu');
+
+navegação.addEventListener('click', () =>{
+    navegação.classList.toggle('ativo');
+    menu.classList.toggle('ativo');
+});
