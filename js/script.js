e65effc62c994d9e98bdaef62769fbc799b91aed
Luidy-Almeida-da-Silva <luidyalmeida00123@gmail.com>
2023-07-05 17:54:46 -0300

    Commit

--- js/script.js
+++ js/script.js
@@ -1,7 +1,21 @@
 const navegação = document.querySelector('.navegação');
 const menu = document.querySelector('.menu');
+const cima = document.querySelector('.fa-brands');
+let timer;
 
 navegação.addEventListener('click', () =>{
     navegação.classList.toggle('ativo');
     menu.classList.toggle('ativo');
 });
+
+function exibirBotão() {
+    cima.classList.add('visivel');
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      cima.classList.remove('visivel');
+    }, 2000);
+  }
+  
+  window.addEventListener('scroll', () =>{
+    exibirBotão()
+  })
